9df0e7f9ae60904378c1b4c87019edf320d52288
Jamil Sinno <jamil.sinno@gmail.com>
2022-07-09 12:17:41 -0300

    added forms, fixed buttons and can add and remove tasks

--- src/App.js
+++ src/App.js
@@ -3,27 +3,42 @@ import { useState } from 'react'
 import React from 'react'
 import Header from './components/Header'
 import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
 
 const App = () => {
+  const [showAddTask, setShowAddTask] = useState(false)
+
   const [tasks, setTask] = useState([{id:1, 
     text:"Test 1", day: 'day 1', reminder: true},
     {id:2, text:"Test 2", day:'day 2', reminder: true},
     {id:3, text:"Test 3", day:'day 3', reminder: false}])
 
-  // Delete Task
+  //Add Task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000 +1)
+    const newTask = {id, ...task}
+
+    setTask([...tasks, newTask])
+  }
+  
+    // Delete Task
   const deleteTask = (id) => {
     setTask(tasks.filter( (task) => task.id !== id ))
   }
 
   //Toggle reminder
   const toggleReminder = (id) => {
-    console.log(id)
+    setTask( tasks.map( (task) => task.id === id ? {...task, reminder: !task.reminder} : task ) )
   }
 
   return (
     <div className="container">
-      <Header />
-      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> : 'No tasks to show'}
+      <Header 
+      onAdd={ () => setShowAddTask(!showAddTask)} 
+      showAdd={showAddTask}
+      />
+      {showAddTask && <AddTask onAdd={addTask}/>}
+      {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>) : ('No tasks to show')}
     </div>
   );
 }
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,11 +1,9 @@
 import PropTypes from 'prop-types'
 
-const Button = ({color, text}) => {
-    const onClick = () => {
-        console.log('click')
-    }
-
-  return <button onClick={onClick} style={{backgroundColor: color}}className='btn'>{text}</button>
+const Button = ({color, text, onClick}) => {
+  return (
+    <button onClick={onClick} style={{backgroundColor: color}}className='btn'>{text}</button>
+  )
 }
 
 Button.defaultProps = {
