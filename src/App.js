2409714b9f8cac59913cfb100bfe69b7faf0b334
Jamil Sinno <jamil.sinno@gmail.com>
2022-07-08 23:22:05 -0300

    added tasks, icons, add and toggle reminder features

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
-function App() {
+import React from 'react'
+import Header from './components/Header'
+import Tasks from './components/Tasks'
+
+const App = () => {
+  const [tasks, setTask] = useState([{id:1, 
+    text:"Test 1", day: 'day 1', reminder: true},
+    {id:2, text:"Test 2", day:'day 2', reminder: true},
+    {id:3, text:"Test 3", day:'day 3', reminder: false}])
+
+  // Delete Task
+  const deleteTask = (id) => {
+    setTask(tasks.filter( (task) => task.id !== id ))
+  }
+
+  //Toggle reminder
+  const toggleReminder = (id) => {
+    console.log(id)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header />
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> : 'No tasks to show'}
     </div>
   );
 }
